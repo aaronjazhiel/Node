20cb91edc5d663e75157dbdfe89d5ac4df497549
adelgago <aaron.jazhiel@hotmail.com>
2018-03-14 00:51:23 -0600

    Se suben los cambios de la practica 3

--- api-restful-favoritos_parte_3/index.js
+++ api-restful-favoritos_parte_3/index.js
@@ -1,11 +1,20 @@
 
 'use strict'
 
+var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3678;
 
-app.listen(port,function(){
-    console.log('API REST FAVORITOS http://localhost:' +port);
+mongoose.connect('mongodb://13.58.210.131:9000',function (err,rest){
+
+if(err){
+    throw err;
+}else {
+
+     console.log('Connetion the database mongo sussefull');
+    app.listen(port,function(){
+        console.log('API REST FAVORITOS http://localhost:' +port +'/favorito');
+    });
+}
+
 });
-
-
